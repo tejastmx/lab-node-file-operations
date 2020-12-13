8857637f9a34fb1b6fcfb2eb24e9080e173a7539
tejas mokal <tejasmokal82gmail,com>
2020-12-13 22:29:27 +0530

    done

--- data.js
+++ data.js
@@ -1,1 +1,10 @@
+function data(name, year, qualification) {
+  userdata = {
+    name: name,
+    Year: year,
+    Qualification: qualification,
+  };
+  return userdata;
+};
 
+module.exports = data
--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+const os = require("os");
+const fs = require("fs");
+const crypto = require("crypto");
 
+const data = require("./data");
+
+var userdata = data("tejas", 2020, "BE");
+var user = os.userInfo();
+const password = crypto
+  .createHmac("sha256", "secret")
+  .update("Prograd")
+  .digest("hex");
+
+fs.appendFileSync("message.txt", "PERSONAL DETAILS" + "\n");
+
+fs.appendFileSync(
+  "message.txt",
+  "name ->                " + userdata.name + "\n"
+);
+fs.appendFileSync(
+  "message.txt",
+  "year ->                " + userdata.Year + "\n"
+);
+fs.appendFileSync(
+  "message.txt",
+  "qualification ->       " + userdata.Qualification + "\n"
+);
+fs.appendFileSync(
+  "message.txt",
+  "username ->            " + user.username + "\n"
+);
+
+fs.appendFileSync("message.txt", "password ->            " + password + "\n");
